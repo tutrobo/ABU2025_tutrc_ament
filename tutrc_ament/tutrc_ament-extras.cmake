36753e715d6f54d47351f2ca5fead788fd191659
paripal <paripal5z34@gmail.com>
2025-05-01 01:29:32 +0900

    remove ament_auto_mod

--- tutrc_ament/tutrc_ament-extras.cmake
+++ tutrc_ament/tutrc_ament-extras.cmake
@@ -1,4 +1,4 @@
-find_package(ament_cmake_auto_mod QUIET REQUIRED)
+find_package(ament_cmake_auto QUIET REQUIRED)
 
 include("${tutrc_ament_DIR}/tutrc_ament_init.cmake")
 include("${tutrc_ament_DIR}/tutrc_ament_final.cmake")
